d6852bb07584f8fbb339b9f01d1b638438cd0d32
ABINAYA MUTHUKRISHNAN <abinayageetha02@gmail.com>
2017-09-02 14:00:19 +0530

    Update Hello.java

--- .gitignore/Hello.java
+++ .gitignore/Hello.java
@@ -1,9 +1,15 @@
+import java.util.Scanner;
 public class Hello {
 	public static void main(String[] args)
 	{
+	Scanner sc=new Scanner(System.in);
+	int num=sc.nextInt();
 	String s ="Hello";
 	int i;
+        if(num>0)
+	{
 	for(i=0;i<5;i++)
-	 System.out.println(s);
+	  System.out.println(s);
+	}
 	}
 }
