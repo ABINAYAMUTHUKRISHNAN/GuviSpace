19d0efb01f0b49f38054e6645337b646547e2595
ABINAYA MUTHUKRISHNAN <abinayageetha02@gmail.com>
2017-09-02 10:23:20 +0530

    Update and rename .gitignore to pos_neg_zero.c

---
+++ .gitignore/pos_neg_zero.c
@@ -0,0 +1,9 @@
+int n;
+printf("Enter a number:");
+scanf("%d",&n);
+if(n>=1)
+  printf("%d is Positive",n);
+else if(n<0)
+  printf("%d is Negative",n);
+else if(n==0)
+  printf(The number is Zero)
