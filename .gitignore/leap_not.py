1a61aa0c5e324acb3497a8414b106696cbcfef4a
ABINAYA MUTHUKRISHNAN <abinayageetha02@gmail.com>
2017-09-02 12:50:54 +0530

    Add files via upload

---
+++ .gitignore/leap_not.py
@@ -0,0 +1,9 @@
+year=int(input("Enter the year:"))
+if year%4==0 or year%400==0:
+    if year%100==0:
+        if year%400==0:
+            print("LEAP YEAR")
+        else:
+            print("NOT A LEAP YEAR")
+    else:
+        print("LEAP YEAR")
