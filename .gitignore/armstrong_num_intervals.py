918075c7638435ac5408ea51a56e3f0747e04642
ABINAYA MUTHUKRISHNAN <abinayageetha02@gmail.com>
2017-09-03 08:48:58 +0530

    Update armstrong_num_intervals.py

--- .gitignore/armstrong_num_intervals.py
+++ .gitignore/armstrong_num_intervals.py
@@ -1,14 +1,16 @@
 num1=input("Enter the first number:")
 num2=input("Enter the sec number:")
-arm_num=0
 for i in range(int(num1),int(num2+1)):
  length=len(i)
  j=i
+ arm_num=0
  while(int(i)!=0):
     rem=int(i)%10
     arm_num+=rem**length
     num1=int(i)/10
  if arm_num==j:
     list1+=[j]
- else:
-    print("No Armstrong Numbers found")
+if len(list1)>0:
+  print(list1)
+else: 
+  print("No Armstron Number found")
