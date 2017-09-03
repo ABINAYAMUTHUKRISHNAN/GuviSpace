9855876367b9289d87d87771f031bc6761c9bca7
ABINAYA MUTHUKRISHNAN <abinayageetha02@gmail.com>
2017-09-03 08:38:08 +0530

    Update sum_using_for.java

--- .gitignore/sum_using_for.java
+++ .gitignore/sum_using_for.java
@@ -1,1 +1,13 @@
 
+import java.util.Scanner;
+public class sum_using_for{
+	public static void main(String[] args)
+	{
+	        Scanner sc=new Scanner(System.in);
+		int num,i,sum=0;
+		num=sc.nextInt();
+		for(i=1;i<=num;i++)
+			sum+=i;
+	        System.out.println(sum);
+	}
+}
