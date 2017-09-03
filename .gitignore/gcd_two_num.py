4a4dff4dd651151a9c26011a9f12df5721a24988
ABINAYA MUTHUKRISHNAN <abinayageetha02@gmail.com>
2017-09-03 13:26:46 +0530

    Update gcd_two_num.py

--- .gitignore/gcd_two_num.py
+++ .gitignore/gcd_two_num.py
@@ -1,10 +1,13 @@
 num1=int(input("Enter the first number:"))
 num2=int(input("Enter the second number:"))
-if num1<num2:
-  n=num1
-else:
-  n=num2
-for i in range(1,n):
+if num1>=1 and num2>=1:
+ if num1<num2:
+   n=num1
+ else:
+   n=num2
+ for i in range(1,n):
   if num1%i==0 and num2%i==0:
     gcd=i
-print(gcd)
+ print(gcd)
+else:
+  print("INVALID")
