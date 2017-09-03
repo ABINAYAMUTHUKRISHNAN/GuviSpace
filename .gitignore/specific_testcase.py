aa8f33d97fe52209d938cd33498f5e4dc7030702
ABINAYA MUTHUKRISHNAN <abinayageetha02@gmail.com>
2017-09-03 12:04:49 +0530

    Update specific_testcase.py

--- .gitignore/specific_testcase.py
+++ .gitignore/specific_testcase.py
@@ -1,9 +1,11 @@
 sum=0
 odd=0
-for i in range(1,16):
-  sum+=i
-for i in range(15,46):
-  if i%2!=0:
-    odd+=i
+num=int(input())
+if num>=1:
+  for i in range(1,16):
+    sum+=i
+  for i in range(15,46):
+    if i%2!=0:
+      odd+=i
 print(sum)
 print(odd)
