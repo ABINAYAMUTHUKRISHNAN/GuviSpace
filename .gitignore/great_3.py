5c5b240d7081856d3a14c3ce8e074bd5a91f918b
ABINAYA MUTHUKRISHNAN <abinayageetha02@gmail.com>
2017-09-06 01:20:42 +0530

    Create great_3.py

---
+++ .gitignore/great_3.py
@@ -0,0 +1,9 @@
+num1=int(input())
+num2=int(input())
+num3=int(input())
+if num1>num2 and num1>num3:
+  print(("Big is %d")%num1)
+elif num2>num3:
+  print(("Big is %d")%num2)
+else:
+  print(("Big is %d")%num3)
